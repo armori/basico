d25b4e7637072f60ad5ab53c54aee7cfb3b01061
armori <xbox360181@live.com>
2018-03-13 14:00:27 +0100

    0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FechaComponent } from './fecha/fecha.component';
+import { ImagenesComponent } from './imagenes/imagenes.component';
+import { MonoarchivoComponent } from './monoarchivo/monoarchivo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FechaComponent,
+    ImagenesComponent,
+    MonoarchivoComponent
   ],
   imports: [
     BrowserModule
